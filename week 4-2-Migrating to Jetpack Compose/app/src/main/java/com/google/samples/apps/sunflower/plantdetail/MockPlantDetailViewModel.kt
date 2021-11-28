cb5091c97784b187eed7d18f3b5b82e1320c4d45
NackUn <king4521@naver.com>
2021-11-28 23:16:22 +0900

    [MigratingCodelab] completed Migrating Codelab

---
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/MockPlantDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.google.samples.apps.sunflower.plantdetail
+
+import com.google.samples.apps.sunflower.data.Plant
+
+class MockPlantDetailViewModel {
+    val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
+}
